ae91e6ba02eeaa0ca4250d4b21f1cde990bbd8b3
shuancc <126139612+shuancc@users.noreply.github.com>
2026-02-20 16:34:41 +0800

    Update get-room.ts

--- pages/api/get-room.ts
+++ pages/api/get-room.ts
@@ -1,17 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import { supabase } from "../../lib/supabase"
+import type { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../lib/supabase";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { room_id } = req.query
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { room_id } = req.query;
   const { data, error } = await supabase
     .from("rooms")
     .select("*")
     .eq("id", room_id)
-    .single()
+    .single();
 
-  if (error) return res.status(500).json({ error: error.message })
-  res.status(200).json({ room: data })
+  if (error) return res.status(500).json({ error: error.message });
+  res.status(200).json({ room: data });
 }
